e78c9c80a066f0fdd3296eb6b502aa7d79bdad04
Andrew <andrew@devnull.uk>
2026-05-19 17:43:00 +0100

    Update eza confguration aliases

--- nixos/darwin/home/shell.nix
+++ nixos/darwin/home/shell.nix
@@ -20,10 +20,10 @@ _: {
       ];
     };
     shellAliases = {
-      ls = "eza --group-directories-first --icons=auto";
-      ll = "eza -lh --group-directories-first --icons=auto --git";
-      la = "eza -lah --group-directories-first --icons=auto";
-      lt = "eza --tree --level=2 --group-directories-first --icons=auto";
+      ls = "eza --group --group-directories-first --icons=auto";
+      ll = "eza -lh --group --group-directories-first --icons=auto --git";
+      la = "eza -lah --group --group-directories-first --icons=auto";
+      lt = "eza --tree --level=2 --group --group-directories-first --icons=auto";
     };
   };
 }
